fb5570e85c1289b854606de64db9e27a5c5c2ddb
Gregory Buzdyk <buzdyk.gregory@gmail.com>
2025-05-06 18:02:18 +0700

    add golang-migrate, add a migration to create users table

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/buzdyk/gophermart
 
 go 1.23.8
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.18.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
---
+++ migrations/migrate.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+  "errors"
+  "fmt"
+  "log"
+
+  "github.com/golang-migrate/migrate/v4"
+  _ "github.com/golang-migrate/migrate/v4/database/postgres"
+  _ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// RunMigrations applies all available migrations to the database
+func RunMigrations(databaseDSN string, migrationsPath string) error {
+  log.Println("Running database migrations...")
+
+  m, err := migrate.New(
+    fmt.Sprintf("file://%s", migrationsPath),
+    databaseDSN,
+  )
+  if err != nil {
+    return fmt.Errorf("failed to create migration instance: %w", err)
+  }
+  
+  if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+    return fmt.Errorf("failed to apply migrations: %w", err)
+  }
+
+  srcErr, dbErr := m.Close()
+  if srcErr != nil {
+    return fmt.Errorf("error closing source: %w", srcErr)
+  }
+  
+  if dbErr != nil {
+    return fmt.Errorf("error closing database: %w", dbErr)
+  }
+
+  log.Println("Migrations applied successfully")
+  
+  return nil
+}
